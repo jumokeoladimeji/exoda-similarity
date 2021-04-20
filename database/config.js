fc5a3b708ffb68aee9c5a67e2e7c936db9c5db4d
jumokeoladimeji <jumoke5ng@yahoo.com>
2021-04-20 07:14:48 +0100

    fix heroku database error

--- database/config.js
+++ database/config.js
@@ -4,6 +4,9 @@ const config = {
   connectionString: process.env.DATABASE_URL,
   max: 10,
   idleTimeoutMillis: 30000,
+  ssl: {
+    rejectUnauthorized: false
+  }
 };
 
 const pool = new pg.Pool(config);
